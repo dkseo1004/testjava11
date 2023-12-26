d29419b5b2eebdc274ab10b1a43bdbff8315e070
dkseo1004 <95399398+dkseo1004@users.noreply.github.com>
2023-12-26 17:41:28 +0900

    Update health.sh

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-# 엔진엑스와 연결되지 않은 포트로 스프링 부트가 잘 수행되었는지 체크
-# 이후 엔진엑스 프록시 설정을 변경 -> switch.sh
 
+
+
+# Get the absolute path of the script
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source script/profile.sh
-source script/switch.sh
 
+# Include source scripts
+source $ABSDIR/script/profile.sh
+source $ABSDIR/script/switch.sh
+
+# Find an idle port
 IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
@@ -21,22 +25,22 @@ do
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
-      echo "> Health check 성공"
-      switch_proxy
-      break
+  then
+    echo "> Health check successful"
+    switch_proxy
+    break
   else
-      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-      echo "> Health check: ${RESPONSE}"
+    echo "> The health check response is unknown or is not running."
+    echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
   then
-    echo "> Health check 실패. "
-    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
+    echo "> Health check failed. "
+    echo "> Deployment will end without connecting to Nginx."
     exit 1
   fi
 
-  echo "> Health check 연결 실패. 재시도..."
+  echo "> Health check connection failed. Retry..."
   sleep 10
 done
