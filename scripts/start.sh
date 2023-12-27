e5afaec9a02c039932028f3896d1eeb35c7baacd
RudySeo <gun100491@gmail.com>
2023-12-27 12:44:10 +0900

    배포 테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ROOT_PATH="/home/ubuntu/cicd"
-JAR="$ROOT_PATH/application.jar"
+# $IDLE_PROFILE을 통해 properties 값을 가져오고 active profile을 지정한다
 
-APP_LOG="$ROOT_PATH/application.log"
-ERROR_LOG="$ROOT_PATH/error.log"
-START_LOG="$ROOT_PATH/start.log"
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
-NOW=$(date +%c)
+REPOSITORY=/home/ubuntu/cicd
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/*.jar $JAR
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/*.jar $REPOSITORY/"
 
-SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+cp $REPOSITORY/*.jar $REPOSITORY/
+
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
