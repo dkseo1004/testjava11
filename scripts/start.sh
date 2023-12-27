accd7d5ffb3bf82987b3c705d1461d62796bcda8
RudySeo <gun100491@gmail.com>
2023-12-27 16:58:29 +0900

    테스트중

--- scripts/start.sh
+++ scripts/start.sh
@@ -18,3 +18,8 @@ nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
 
+echo "> $JAR 를 profile=$IDLE_PROFILE 로 실행합니다."
+nohup java -jar \
+  -Dspring.profiles.active=$IDLE_PROFILE \
+  $JAR > $REPOSITORY/nohup.out 2>&1 &
+
