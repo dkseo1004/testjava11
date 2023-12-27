10cfda97f9eb42e9b843f3f250b1b214af6b1aab
RudySeo <gun100491@gmail.com>
2023-12-27 16:45:07 +0900

    테스트

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+ROOT_PATH="/home/ubuntu/cicd"
+JAR="$ROOT_PATH/application.jar"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
-IDLE_PORT=$(find_idle_port)
-
-echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
-
-if [ -z ${IDLE_PID} ]
-then
-  echo "> 현재 구동 중인 애플리케이션이 없습니다."
+if [ -z "$SERVICE_PID" ]; then
+  echo "서비스 NouFound" >> $STOP_LOG
 else
-  echo "kill -15 $IDLE_PID"
-  kill -15 ${IDLE_PID}
-  sleep 5
+  echo "서비스 종료 " >> $STOP_LOG
+  kill "$SERVICE_PID"
+  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
