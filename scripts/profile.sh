b1d5ae73de7f1bd827d5bc73ed7c07dd3101843d
RudySeo <gun100491@gmail.com>
2023-12-27 12:52:00 +0900

    테스트중

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env bash
 
-# bash는 값을 리턴할 수 없으므로 echo로 결과를 출력하여 클라이언트에서 값을 사용한다
-
 # 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
-{
-    # 현재 엔진엑스가 바라보는 스프링 부트의 정상 수행 여부 확인 -> HttpStatus
+{   # 현재 엔진엑스가 바라보고 있는 스프링부트가 정상적으로 수행 중인지 확인.
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
-    then
-        CURRENT_PROFILE=real2 # 정상적으로 수행 중이지 않을 경우
+    then # 오류가 발생하면 real2를 현재 profile로 사용
+        CURRENT_PROFILE=dev2
     else
         CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
-    if [ ${CURRENT_PROFILE} == real1 ]
-    then
-      IDLE_PROFILE=real2 # 엔진엑스랑 연결되지 않은 profile
+    if [ ${CURRENT_PROFILE} == dev ]
+    then # IDLE_PROFILE 은 엔진엑스와 연결되지 않은 프로필 , 스프링 부트 프로젝트를 이 프로필로 연결
+      IDLE_PROFILE=dev2
     else
-      IDLE_PROFILE=real1
+      IDLE_PROFILE=dev
     fi
 
-    echo "${IDLE_PROFILE}" # 마지막에 echo를 통해 출력하여 클라이언트가 값을 사용할 수 있도록 한다
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
+    echo "${IDLE_PROFILE}"
 }
 
 # 쉬고 있는 profile의 port 찾기
@@ -30,10 +28,10 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real1 ]
+    if [ ${IDLE_PROFILE} == dev ]
     then
-      echo "8081"
+      echo "8080"
     else
-      echo "8082"
+      echo "8081"
     fi
 }
