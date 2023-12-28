9c3632fe441bc333c99b85f953acefae606a2119
RudySeo <gun100491@gmail.com>
2023-12-28 12:34:41 +0900

    크크

--- src/main/java/com/example/githubtest/ProfileController.java
+++ src/main/java/com/example/githubtest/ProfileController.java
@@ -16,23 +16,15 @@ public class ProfileController {
 	private final Environment env;
 
 	@GetMapping("/profile")
-	public String profile() {
-		List<String> profiles = Arrays.asList(env.getActiveProfiles());
-		List<String> realProfiles = Arrays.asList("real", "real1", "real2");
-		String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+	public String getProfile() {
+		List<String> profile = Arrays.asList(env.getActiveProfiles());
+		List<String> realProfiles = Arrays.asList("set1", "set2");
+		String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
 
-		return profiles.stream()
+		return profile.stream()
 			.filter(realProfiles::contains)
 			.findAny()
 			.orElse(defaultProfile);
 	}
 
-	@RestController
-	public class HealthController {
-
-		@GetMapping("/health")
-		public String checkHealth() {
-			return "healthy";
-		}
-	}
 }
