c28d20d71a9143abe6b5654ff757d5bebdec3d76
RudySeo <gun100491@gmail.com>
2023-12-28 12:31:17 +0900

    테스트중

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ROOT_PATH="/home/ubuntu/cicd"
-JAR="$ROOT_PATH/application.jar"
-STOP_LOG="$ROOT_PATH/stop.log"
-SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+# 기존 Eginx엔스에 연결되어 있지 않지만, 실행 중이던 스프링 부트 종료
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH) # 현재 stop.sh가 속해있는 경로
+source ${ABSDIR}/profile.sh # 해당 코드로 profile.sh 내의 함수 사용
 
-if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NouFound" >> $STOP_LOG
+IDLE_PORT=$(find_idle_port)
+
+echo ">>> $IDLE_PORT 에서 구동중인 애플리케이션 PID 확인"
+IDLE_PID=$(sudo lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
+then
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "서비스 종료 " >> $STOP_LOG
-  kill "$SERVICE_PID"
-  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+  # # Nginx에 연결되어 있지는 않지만 현재 실행 중인 jar 를 Kill 합니다.
+  echo ">>> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
 fi
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
-CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
-TARGET_PORT=0
 
-echo "Nginx currently proxies to ${CURRENT_PORT}."
+# Ngnix가 바라보는 스프링 부트를 최신 버전으로 변경
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
-if [ ${CURRENT_PORT} -eq 8081 ]; then
-  TARGET_PORT=8082
-elif [ ${CURRENT_PORT} -eq 8082 ]; then
-  TARGET_PORT=8081
-else
-  echo "No WAS is connected to nginx"
-  exit 1
-fi
+function switch_proxy() {
+    IDLE_PORT=$(find_idle_port)
 
-echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
-
-echo "Now Nginx proxies to ${TARGET_PORT}."
-sudo service nginx reload
-
-echo "Nginx reloaded."
+    echo ">>> 전환할 Port: $IDLE_PORT"
+    echo ">>> Port 전환"
+    # 아래 줄은 echo를 통해서 나온 결과를 | 파이프라인을 통해서 service-url.inc에 덮어쓸 수 있습니다.
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo ">>> Reload Nginx"
+    sudo service nginx reload # Nginx reload를 합니다.
+}
