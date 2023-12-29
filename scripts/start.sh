d58da05ac4d229dfc2dd7e7c938f034b182ab02c
RudySeo <gun100491@gmail.com>
2023-12-29 10:41:39 +0900

    테스트 중

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,32 +1,29 @@
 #!/usr/bin/env bash
 
-# $IDLE_PROFILE을 통해 properties 값을 가져오고 active profile을 지정한다
-
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ubuntu/app
+REPOSITORY=/home/ubuntu/cicd
+PROJECT_NAME=githbtest
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/build/libs/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY//build/libs/*.jar $REPOSITORY/"
+cp $REPOSITORY//build/libs/*.jar $REPOSITORY/
 
-cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
-
-echo "> 새 어플리케이션 배포"
+echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name: $JAR_NAME"
+echo "> JAR_NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
-
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다"
 nohup java -jar \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.profiles.active=$IDLE_PROFILE \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
