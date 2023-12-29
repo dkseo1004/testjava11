2149f933036ea4b23830280bd8e1457fc66a78b2
RudySeo <gun100491@gmail.com>
2023-12-29 12:44:00 +0900

    크크

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,40 +1,30 @@
 #!/usr/bin/env bash
 
-# Nginx와 연결되지 않은 포트로 스프링 부트가 잘 수행되었는지 체크
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
-source ${ABSDIR}/switch.sh
+source ${ABSDIR}/profile.sh   # import profile.sh
 
-IDLE_PORT=$(find_idle_port)
+REPOSITORY=/home/ubuntu/cicd
 
-echo "> Health Check Start!"
-echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://localhost:$IDLE_PORT/profile "
-sleep 10
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/build/libs/*.jar $REPOSITORY/"
 
-for RETRY_COUNT in {1..10}
-do
-  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
-  if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
-      echo "> Health check 성공"
-      switch_proxy # 잘 떳다면, switch.sh의 switch_proxy로 Nginx 프록시 설정을 변경
-      break
-  else
-      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-      echo "> Health check: ${RESPONSE}"
-  fi
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)    # jar 이름 꺼내오기
 
-  if [ ${RETRY_COUNT} -eq 10 ]
-  then
-    echo "> Health check 실패. "
-    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
-    exit 1
-  fi
+echo "> JAR Name: $JAR_NAME"
 
-  echo "> Health check 연결 실패. 재시도..."
-  sleep 10
-done
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+nohup java -jar \
+    -Dspring.profiles.active=$IDLE_PROFILE \   # 위에서 보았던 것처럼 $IDLE_PROFILE에는 real1 or real2가 반환되는데 반환되는 properties를 실행한다는 뜻입니다.
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
