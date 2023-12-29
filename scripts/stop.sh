d1138261650b2cf175377e1c87ab7ebf974946a4
dkseo1004 <95399398+dkseo1004@users.noreply.github.com>
2023-12-29 11:30:07 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-#기존 엔진엑스에 연결되어 있진 않지만, 실행 중이던 스프링 부트 종료
-
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)  # stop.sh가 속해있는 경로를 찾는다.
-source ${ABSDIR}/profile.sh
+ABSDIR=$(dirname $ABSPATH) # 현재 stop.sh가 속한 경로
+source ${ABSDIR}/profile.sh # import 구문, profile.sh의 여러 function 사용 가능
 
 IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
-  sleep 10 # 종료 후 10초 대기 (종료 시간은 애플리케이션에 따라 조절 가능)
+  sleep 5
 fi
