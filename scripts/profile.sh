c6af59a1f7e66b461a4b73523bab992bdb940476
RudySeo <gun100491@gmail.com>
2023-12-30 17:17:54 +0900

    ㅋmzmzm

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -2,13 +2,13 @@
 
 
 function find_idle_profile() {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://15.165.59.117/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://15.165.59.117/test)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400보다 크면 (즉, 40x/ 50x 에러 모두 포함)
     then
         CURRENT_PROFILE=set2
     else
-        CURRENT_PROFILE=$(curl -s http://15.165.59.117/profile)
+        CURRENT_PROFILE=$(curl -s http://15.165.59.117/test)
     fi
 
     if [ "${CURRENT_PROFILE}" == "set1" ]
