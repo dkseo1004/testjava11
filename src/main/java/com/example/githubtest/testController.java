ee1dbdec330084dbc69aa60d017f220d78bf1bbb
RudySeo <gun100491@gmail.com>
2023-12-31 04:51:32 +0900

    테스트11

--- src/main/java/com/example/githubtest/testController.java
+++ src/main/java/com/example/githubtest/testController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
 
-	@GetMapping("/")
+	@GetMapping("/test")
 	public String test(){
 			return "안녕하";
 	}
