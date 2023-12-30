b7f34dd3d8a5b7f62858ec85fbc156c08090edd3
RudySeo <gun100491@gmail.com>
2023-12-30 22:25:02 +0900

    테스트

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+ABSDIR=$(dirname $ABSPATH) # 현재 stop.sh가 속한 경로
+source ${ABSDIR}/profile.sh # import 구문, profile.sh의 여러 function 사용 가능
 
 IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(sudo lsof -ti tcp:${IDLE_PORT})
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $IDLE_PID"   # Nginx에 연결되어 있지는 않지만 현재 실행 중인 jar 를 Kill 합니다.
+  echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
   sleep 5
 fi
