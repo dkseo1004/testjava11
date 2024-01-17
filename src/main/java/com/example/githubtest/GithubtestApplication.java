18f8c644ed5ea2651defaf812c8e54d98d399f90
RudySeo <gun100491@gmail.com>
2024-01-17 17:49:35 +0900

    스프링 배치 테스트

--- src/main/java/com/example/githubtest/GithubtestApplication.java
+++ src/main/java/com/example/githubtest/GithubtestApplication.java
@@ -1,9 +1,12 @@
 package com.example.githubtest;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
+
 public class GithubtestApplication {
 
 	public static void main(String[] args) {
